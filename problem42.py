30112792420e6724b10b35a655652461c76dbc57
lordofsraam <lordofsraam@gmail.com>
2020-05-19 12:37:40 -0400

    problem 42

---
+++ problem42.py
@@ -0,0 +1,14 @@
+print(
+    sum(
+        1 for _ in filter(
+            lambda w: w in ((i/2)*(i+1) for i in range(20)),
+            map(
+                lambda x: sum(map(lambda y: ord(y)-64, x)),
+                map(
+                    lambda n: n[1:-1],
+                    open('p042_words.txt').read().split(',')
+                )
+            )
+        )
+    )
+)
